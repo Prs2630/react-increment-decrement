b6b0e223e495d03c98ed9f86bcb33a9a01c8a099
Prs2630 <poojakinholkar1998@gmail.com>
2022-01-16 09:56:49 +0530

    react increment decrement app

---
+++ hello-vite/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Lncr from './Lncr'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+  <Lncr/>
+            
+    </div>
+  )
+}
+
+export default App
